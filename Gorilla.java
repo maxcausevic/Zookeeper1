38ccf6de4805105238034b1537e55e47f8a1d067
Maxine Causevic <maxine@fitmaxpersonaltraining.com>
2021-04-05 19:36:13 -0500

    first commit

---
+++ Gorilla.java
@@ -0,0 +1,17 @@
+class Gorilla extends Mammal {
+    
+    public int throwSomething(){
+        System.out.println("The gorilla has thrown something.");
+        return energyLevel -= 3;
+    }
+    public int eatBananas(){
+        System.out.println("The gorilla is eating bananas");
+        return energyLevel += 10;
+
+    }
+    public int climb(){
+        System.out.println("The gorilla climbed");
+        return energyLevel -= 10;
+    }
+}
+
---
+++ Mammal.java
@@ -0,0 +1,6 @@
+class Mammal{
+    public int energyLevel = 100;
+    public void displayEnergy() {
+        System.out.println("Energy level is" + energyLevel);
+    }
+}
